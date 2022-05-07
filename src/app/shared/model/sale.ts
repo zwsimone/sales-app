dbb04e0457b7f1f13ffb56ed66603e5c53e29009
zwsimone <zwsimone@gmail.com>
2022-05-07 22:12:49 +0200

    Add Sales class

--- src/app/shared/model/sale.ts
+++ src/app/shared/model/sale.ts
@@ -77,4 +77,100 @@ export class Sale {
             serverName: null
         };
     }
+
+    getTips(): number {
+        return (this.paymentTotal - this.orderTotal);
+    }
+}
+
+export class Sales {
+    list: Sale[];
+
+    constructor() { }
+
+    getTotalNumberOrders(): number {
+        return this.list.length;
+    }
+
+    getTotalEFT(): number {
+        const eftList = this.list.filter(sale => sale.paymentMethod === "eft");
+        let eftTotal = 0;
+        eftList.forEach(sale => eftTotal += sale.paymentTotal);
+        return eftTotal;
+    }
+
+    getTotalCash(): number {
+        const cashList = this.list.filter(sale => sale.paymentMethod === "cash");
+        let cashTotal = 0;
+        cashList.forEach(sale => cashTotal += sale.paymentTotal);
+        return cashTotal;
+    }
+
+    getTotalCard(): number {
+        const cardList = this.list.filter(sale => sale.paymentMethod === "card");
+        let cardTotal = 0;
+        cardList.forEach(sale => cardTotal += sale.paymentTotal);
+        return cardTotal;
+    }
+
+    getFinalTotal(): number {
+        let total = 0;
+        this.list.forEach(sale => total += sale.paymentTotal);
+        return total;
+    }
+
+    getTotalDeliveryFees(): number {
+        let fees = 0;
+        this.list.forEach(sale => fees += sale.deliveryFee);
+        return fees;
+    }
+
+    getTotalTips(): number {
+        let tips = 0;
+        this.list.forEach(sale => tips += sale.getTips());
+        return tips;
+    }
+
+    getSalesByServer(name: string): Sale[] {
+        return this.list.filter(sale => sale.serverName === name);
+    }
+
+    getTotalOrdersByServer(name: string): number {
+        let total = 0;
+        this.list.forEach(sale => {
+            if (sale.serverName === name) total += sale.paymentTotal
+        });
+        return total;
+    }
+
+    getTotalDeliveryFeesByServer(name: string): number {
+        let fees = 0;
+        this.list.forEach(sale => {
+            if (sale.serverName === name) fees += sale.deliveryFee
+        });
+        return fees;
+    }
+
+    getTotalTipsByServer(name: string): number {
+        let tips = 0;
+        this.list.forEach(sale => {
+            if (sale.serverName === name) tips += sale.getTips()
+        });
+        return tips;
+    }
+
+    getCommissionByServer(name: string): number {
+        return 0.025 * ((this.getTotalOrdersByServer(name) - this.getTotalDeliveryFeesByServer(name)) - (0.2 * (this.getTotalOrdersByServer(name) - this.getTotalDeliveryFeesByServer(name))));
+    }
+
+    getFinalTotalByServer(name: string): number {
+        return this.getTotalOrdersByServer(name) + this.getCommissionByServer(name) + this.getTotalDeliveryFeesByServer(name) + this.getTotalTipsByServer(name);
+    }
+
+    getServers(): string[] {
+        let serverString = "";
+        this.list.forEach(sale => serverString += sale.serverName + ",");
+        const serverList = serverString.split(',');
+        return serverList.filter((v, i, a) => a.indexOf(v) === i);
+    }
 }
